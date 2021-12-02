06b82fed37ed4328832203c5c532850e441eb909
Lukas <helibum1@gmail.com>
2021-12-02 20:18:11 +0100

    removed unecessary stuff

--- src/app/shared/components/rules-list/rules-list.component.ts
+++ src/app/shared/components/rules-list/rules-list.component.ts
@@ -26,14 +26,6 @@ export class RulesListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.markdownService.renderer.heading = (text: string, level: number) => {
-      const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
-      return '<h' + level + '>' +
-               '<a name="' + escapedText + '" class="anchor" href="#' + escapedText + '">' +
-                 '<span class="header-link"></span>' +
-               '</a>' + text +
-             '</h' + level + '>';
-    };
   }
 
 }
